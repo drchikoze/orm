e157b2be93bb9324bd0b18ca5c1d16c8e080d440
tropick <=>
2014-08-15 11:19:17 +0300

    add save method

--- entity.py
+++ entity.py
@@ -1,7 +1,10 @@
+import psycopg2
+
 class DatabaseError(Exception):
     pass
 class NotFoundError(Exception):
     pass
+
 
 class Entity(object):
     db = None
@@ -16,35 +19,74 @@ class Entity(object):
     __update_query    = 'UPDATE "{table}" SET {columns} WHERE {table}_id=%s'
 
     def __init__(self, id=None):
+        self.__class__.db = psycopg2.connect( user='postgres', password = 'Vb094545');
         if self.__class__.db is None:
             raise DatabaseError()
 
         self.__cursor   = self.__class__.db.cursor()
         self.__fields   = {}
         self.__id       = id
-        self.__loaded   = False
+        self.__loaded   = False 
         self.__modified = False
         self.__table    = self.__class__.__name__.lower()
 
     def __getattr__(self, name):
-        # check, if instance is modified and throw an exception
-        # get corresponding data from database if needed
+        # if self.__modified == True:
+        #     raise DatabaseError()
+        # # # check, if instance is modified and throw an exception
+        # # # get corresponding data from database if needed
+        # if self.__id is None:
+        #     print "set id pls"
+        #     return None
+        # if self.__loaded == False:
+        #     query = self.__class__.__select_query
+        #     args = [self.__table]
+        #     args.add(self.__table)
+        #     self.__execute_query(query, args):
+        #     try:
+        #         self.__cursor.execute(self.__class__.__select_query.format(self.__table), self.__id)
+        #         values = self.__cursor.fetchall()
+        #         self.__cursor.execute('SELECT column_name FROM information_schema.columns WHERE table_name = '{}';'.format(self.__table))
+        #         keys = self.__cursor.fetchall()
+        #         counter = 0
+        #         for key in keys:
+        #             self.__fields[key[0]] = values[0][counter]
+        #             counter += 1
+        #         self.__loaded = True
+
+
+
+
         # check, if requested property name is in current class
         #    columns, parents, children or siblings and call corresponding
         #    getter with name as an argument
         # throw an exception, if attribute is unrecognized
-        # sfds
         pass
 
     def __setattr__(self, name, value):
         # check, if requested property name is in current class
+        if hasattr(self, name):
+            self.__dict__['name'] = value
+        else:
+            super(Entity, self).__setattr__(name, value)
+
         #    columns, parents, children or siblings and call corresponding
         #    setter with name and value as arguments or use default implementation
-        pass
 
-    def __execute_query(self, query, args):
+    def __execute_query(self, query, args = None):
         # execute an sql statement and handle exceptions together with transactions
-        pass
+        try:
+            if args is not None:
+                self.__cursor.execute(query,args)
+            else:
+                self.__cursor.execute(query)
+        except psycopg2.DatabaseError, e:
+            print 'Error: %s' % e    
+            sys.exit(1)
+        finally:
+            if con:
+                self.__class__.db.commit()
+                self.__class__.db.close()
 
     def __insert(self):
         # generate an insert query string from fields keys and values and execute it
@@ -53,7 +95,6 @@ class Entity(object):
         pass
 
     def __load(self):
-        # if current instance is not loaded yet — execute select statement and store it's result as an associative array (fields), where column names used as keys
         pass
 
     def __update(self):
@@ -104,8 +145,7 @@ class Entity(object):
 
     @property
     def id(self):
-        # try to guess yourself
-        pass
+        return self.__id
 
     @property
     def created(self):
@@ -114,9 +154,31 @@ class Entity(object):
 
     @property
     def updated(self):
-        # try to guess yourself
-        pass
+        return self.__update
 
     def save(self):
         # execute either insert or update query, depending on instance id
-        pass
+        if self.__modified == True:
+            columns_set = []
+            for key in self.__fields.keys():
+                tmp = str(key) + " = " + str(self.__fields[key])
+                columns_set.append(tmp)
+            columns = ", ".join(columns_set)
+            updt_query = self.__class__.__update_query.format(columns)
+            self.__execute_query(updt_query, self.__id)
+        else:
+            columns_set = []
+            placeholders_set = []
+            for value in self.__fields.values():
+                placeholders_set.append(value)
+            placeholders = ", ".join(placeholders_set)
+            for key in self.__fields.keys():
+                columns_set.append(key)
+            columns = ", ".join(columns_set)
+            save_query = self.__class__.__insert_query.format(self.__table, columns, placeholders)
+            self.__execute_query(save_query)
+        self.__loaded = True
+
+
+first = Entity()
+# print first.save()
